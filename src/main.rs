9c52611cd004ec8d5e49116b4babf61d8af02877
Tglman <tglman@tglman.com>
2022-02-28 15:45:54 +0000

    feature: add external config

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 use actix_files::Files;
 use actix_web::{middleware::Logger, App, HttpServer};
+use serde_derive::Deserialize;
 
+#[derive(Deserialize)]
 struct Config {
     static_path: String,
+    listen_port: u16,
+    listen_host: String,
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
-    let config = Config {
-        static_path: "/var/www/example-project/".to_owned(),
-    };
+    let config = envy::from_env::<Config>().unwrap();
+
     log::info!("starting HTTP server at http://localhost:8080");
 
     HttpServer::new(move || {
@@ -18,7 +21,7 @@ async fn main() -> std::io::Result<()> {
             .service(Files::new("/", &config.static_path).index_file("index.html"))
             .wrap(Logger::default())
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind((config.listen_host, config.listen_port))?
     .run()
     .await
 }
